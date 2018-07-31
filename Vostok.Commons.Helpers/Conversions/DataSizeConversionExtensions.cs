d699fd63e2d379c2d19549eac6f6db540caa576c
razboynikov <razboynikov@skbkontur.ru>
2018-07-31 18:51:02 +0500

    Added all files.

---
+++ Vostok.Commons.Helpers/Conversions/DataSizeConversionExtensions.cs
@@ -0,0 +1,74 @@
+namespace Vostok.Commons.Helpers.Conversions
+{
+    public static class DataSizeConversionExtensions
+    {
+        public static DataSize Bytes(this ushort value) =>
+            DataSize.FromBytes(value);
+
+        public static DataSize Bytes(this int value) =>
+            DataSize.FromBytes(value);
+
+        public static DataSize Bytes(this long value) =>
+            DataSize.FromBytes(value);
+
+        public static DataSize Kilobytes(this ushort value) =>
+            DataSize.FromKilobytes(value);
+
+        public static DataSize Kilobytes(this int value) =>
+            DataSize.FromKilobytes(value);
+
+        public static DataSize Kilobytes(this long value) =>
+            DataSize.FromKilobytes(value);
+
+        public static DataSize Kilobytes(this double value) =>
+            DataSize.FromKilobytes(value);
+
+        public static DataSize Megabytes(this ushort value) =>
+            DataSize.FromMegabytes(value);
+
+        public static DataSize Megabytes(this int value) =>
+            DataSize.FromMegabytes(value);
+
+        public static DataSize Megabytes(this long value) =>
+            DataSize.FromMegabytes(value);
+
+        public static DataSize Megabytes(this double value) =>
+            DataSize.FromMegabytes(value);
+
+        public static DataSize Gigabytes(this ushort value) =>
+            DataSize.FromGigabytes(value);
+
+        public static DataSize Gigabytes(this int value) =>
+            DataSize.FromGigabytes(value);
+
+        public static DataSize Gigabytes(this long value) =>
+            DataSize.FromGigabytes(value);
+
+        public static DataSize Gigabytes(this double value) =>
+            DataSize.FromGigabytes(value);
+
+        public static DataSize Terabytes(this ushort value) =>
+            DataSize.FromTerabytes(value);
+
+        public static DataSize Terabytes(this int value) =>
+            DataSize.FromTerabytes(value);
+
+        public static DataSize Terabytes(this long value) =>
+            DataSize.FromTerabytes(value);
+
+        public static DataSize Terabytes(this double value) =>
+            DataSize.FromTerabytes(value);
+
+        public static DataSize Petabytes(this ushort value) =>
+            DataSize.FromPetabytes(value);
+
+        public static DataSize Petabytes(this int value) =>
+            DataSize.FromPetabytes(value);
+
+        public static DataSize Petabytes(this long value) =>
+            DataSize.FromPetabytes(value);
+
+        public static DataSize Petabytes(this double value) =>
+            DataSize.FromPetabytes(value);
+    }
+}
---
+++ Vostok.Commons.Helpers/TimeBudget.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using Vostok.Commons.Helpers.Extensions;
+
+namespace Vostok.Commons.Helpers
+{
+    public class TimeBudget
+    {
+        public static readonly TimeBudget Infinite = new TimeBudget(TimeSpan.FromHours(1));
+        public static readonly TimeBudget Expired = new TimeBudget(TimeSpan.Zero);
+
+        public static TimeBudget StartNew(TimeSpan budget, TimeSpan precision) =>
+            new TimeBudget(budget, precision).Start();
+
+        public static TimeBudget StartNew(TimeSpan budget) =>
+            new TimeBudget(budget).Start();
+
+        public static TimeBudget StartNew(int budgetMs, int precisionMs) =>
+            new TimeBudget(TimeSpan.FromMilliseconds(budgetMs), TimeSpan.FromMilliseconds(precisionMs)).Start();
+
+        public static TimeBudget StartNew(int budgetMs) =>
+            new TimeBudget(TimeSpan.FromMilliseconds(budgetMs)).Start();
+
+        private readonly TimeSpan budget;
+        private readonly TimeSpan precision;
+        private readonly Stopwatch watch;
+
+        public TimeBudget(TimeSpan budget, TimeSpan precision)
+        {
+            this.budget = budget;
+            this.precision = precision;
+            watch = new Stopwatch();
+        }
+
+        public TimeBudget(TimeSpan budget)
+            : this(budget, TimeSpan.FromMilliseconds(5))
+        {
+        }
+
+        public TimeSpan Budget => budget;
+
+        public TimeSpan Precision => precision;
+
+        public TimeBudget Start()
+        {
+            watch.Start();
+            return this;
+        }
+
+        public TimeSpan Remaining()
+        {
+            var remaining = budget - watch.Elapsed;
+            return remaining < precision
+                ? TimeSpan.Zero
+                : remaining;
+        }
+
+        public TimeSpan Elapsed() => watch.Elapsed;
+
+        public TimeSpan TryAcquireTime(TimeSpan neededTime) =>
+            TimeSpanExtensions.Min(neededTime, Remaining());
+
+        public bool HasExpired() => Remaining() <= TimeSpan.Zero;
+    }
+}
