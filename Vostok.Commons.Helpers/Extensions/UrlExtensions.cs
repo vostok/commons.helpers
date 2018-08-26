a5ea22593c3ec4ee836605cf07f8fec58a38ebc2
n.kochnev <n.kochnev@skbkontur.ru>
2018-08-26 22:47:39 +0500

    added UrlExtension and UrlNormalizer

---
+++ Vostok.Commons.Helpers/Extensions/UrlExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vostok.Commons.Helpers.Extensions
+{
+    internal static class UrlExtensions
+    {
+        public static string ToStringWithoutQuery(this Uri url)
+        {
+            var urlString = url.ToString();
+
+            var queryBeginning = urlString.IndexOf("?", StringComparison.Ordinal);
+            if (queryBeginning >= 0)
+                urlString = urlString.Substring(0, queryBeginning);
+
+            return urlString;
+        }
+    }
+}
