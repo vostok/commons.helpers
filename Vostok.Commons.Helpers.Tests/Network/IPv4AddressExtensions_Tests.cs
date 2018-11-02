fe1971bc1a06d75c29d37aec23ea44c5fad7fb28
epeshk <peshkov@skbkontur.ru>
2018-11-02 14:52:24 +0500

    add windows-specific arp cache helper

---
+++ Vostok.Commons.Helpers.Tests/Network/IPv4AddressExtensions_Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Commons.Helpers.Network;
+
+namespace Vostok.Commons.Helpers.Tests.Network
+{
+    [TestFixture]
+    internal class IPv4AddressExtensions_Tests
+    {
+        [Test]
+        public void ToUInt32_should_work_correctly()
+        {
+            var address = IPAddress.Parse("46.17.203.102");
+            
+            address.ToUInt32().Should().Be(BitConverter.ToUInt32(address.GetAddressBytes().Reverse().ToArray(), 0));
+        }
+
+        [Test]
+        public void ToUInt32BigEndian_should_work_correctly()
+        {
+            var address = IPAddress.Parse("46.17.203.102");
+
+            address.ToUInt32BigEndian().Should().Be(BitConverter.ToUInt32(address.GetAddressBytes(), 0));
+        }
+
+        [Test]
+        public void ToUInt32_should_throw_for_non_IPv4_addresses()
+        {
+            var address = IPAddress.IPv6Loopback;
+
+            new Action(() => address.ToUInt32()).Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ToUInt32BigEndian_should_throw_for_non_IPv4_addresses()
+        {
+            var address = IPAddress.IPv6Loopback;
+
+            new Action(() => address.ToUInt32BigEndian()).Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ Vostok.Commons.Helpers/Network/IPv4AddressExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Vostok.Commons.Helpers.Network
+{
+    internal static class IPv4AddressExtensions
+    {
+        public static uint ToUInt32(this IPAddress address)
+        {
+            if (address.AddressFamily != AddressFamily.InterNetwork)
+                throw new ArgumentException("Address must be an IPv4 address.", nameof(address));
+
+            var rawAddress = (uint) address.Address;
+
+            return
+                ((rawAddress & 0x000000FFU) << 24) |
+                ((rawAddress & 0x0000FF00U) << 8) |
+                ((rawAddress & 0x00FF0000U) >> 8) |
+                ((rawAddress & 0xFF000000U) >> 24);
+        }
+
+        public static uint ToUInt32BigEndian(this IPAddress address)
+        {
+            if (address.AddressFamily != AddressFamily.InterNetwork)
+                throw new ArgumentException("Address must be an IPv4 address.", nameof(address));
+
+            return (uint) address.Address;
+        }
+    }
+}
