9f8791ea9bc6c955b077af83a304427a9d33b071
iloktionov <iloktionov@skbkontur.ru>
2019-01-23 12:13:52 +0500

    Added FreeTcpPortFinder.

---
+++ Vostok.Commons.Helpers/Network/FreeTcpPortFinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Vostok.Commons.Helpers.Network
+{
+    /// <summary>
+    /// Helper class to discover free TCP ports.
+    /// </summary>
+    internal static class FreeTcpPortFinder
+    {
+        /// <summary>
+        /// Returns a currently available TCP port to bind on.
+        /// </summary>
+        public static int GetFreePort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            try
+            {
+                listener.Start();
+                return ((IPEndPoint) listener.LocalEndpoint).Port;
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
