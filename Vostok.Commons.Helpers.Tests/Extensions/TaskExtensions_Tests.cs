2b8a22d3b4aae5801ff49db45c7c5e6dc0583875
kungurtsev <kungurtsev@skbkontur.ru>
2019-03-13 12:30:36 +0500

    Task.WaitAsync

---
+++ Vostok.Commons.Helpers.Tests/Extensions/TaskExtensions_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using Vostok.Commons.Helpers.Extensions;
+using Vostok.Commons.Testing;
+
+namespace Vostok.Commons.Helpers.Tests.Extensions
+{
+    [TestFixture]
+    internal class TaskExtensions_Tests
+    {
+        [Test]
+        public void WaitAsync_should_wait_for_task()
+        {
+            var task = Task.Delay(0.5.Seconds());
+            task.WaitAsync(10.Seconds()).ShouldCompleteIn(1.Seconds()).Should().BeTrue();
+        }
+
+        [Test]
+        public void WaitAsync_should_wait_with_timeout()
+        {
+            var task = Task.Delay(10.Seconds());
+            task.WaitAsync(0.5.Seconds()).ShouldCompleteIn(1.Seconds()).Should().BeFalse();
+            task.ShouldNotCompleteIn(0.5.Seconds());
+        }
+    }
+}
--- Vostok.Commons.Helpers/Extensions/TaskExtensions.cs
+++ Vostok.Commons.Helpers/Extensions/TaskExtensions.cs
@@ -1,9 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Vostok.Commons.Helpers.Extensions
 {
     internal static class TaskExtensions
     {
         public static Task SilentlyContinue(this Task source) => source.ContinueWith(_ => {});
+
+        /// <summary>
+        /// <para>Returns true if <paramref name="task"/> finished within given <paramref name="timeout"/>.</para>
+        /// <para>Does not stop or cancel given <paramref name="task"/></para>
+        /// </summary>
+        public static async Task<bool> WaitAsync(this Task task, TimeSpan timeout)
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                var delay = Task.Delay(timeout, cts.Token);
+
+                var result = await Task.WhenAny(task, delay).ConfigureAwait(false);
+                if (result == delay)
+                {
+                    return false;
+                }
+
+                cts.Cancel();
+            }
+
+            return true;
+        }
     }
 }
