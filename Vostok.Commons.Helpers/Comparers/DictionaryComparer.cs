90d60ba722deab2b0aaf4e89a27738989b46b8f8
iloktionov <iloktionov@skbkontur.ru>
2019-03-25 13:07:56 +0500

    + ListComparer, DictionaryComparer

---
+++ Vostok.Commons.Helpers/Comparers/DictionaryComparer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vostok.Commons.Helpers.Comparers
+{
+    internal class DictionaryComparer<TKey, TValue> : IEqualityComparer<IReadOnlyDictionary<TKey, TValue>>
+    {
+        private readonly IEqualityComparer<TValue> valueComparer;
+
+        public DictionaryComparer(IEqualityComparer<TValue> valueComparer = null)
+        {
+            this.valueComparer = valueComparer ?? EqualityComparer<TValue>.Default;
+        }
+
+        public bool Equals(IReadOnlyDictionary<TKey, TValue> x, IReadOnlyDictionary<TKey, TValue> y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            if (x.Count != y.Count)
+                return false;
+
+            foreach (var pair in x)
+            {
+                if (!y.TryGetValue(pair.Key, out var yValue) || !valueComparer.Equals(pair.Value, yValue))
+                    return false;
+            }
+
+            return true;
+        }
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        public int GetHashCode(IReadOnlyDictionary<TKey, TValue> dictionary)
+            => dictionary == null ? 0 : dictionary.Aggregate(dictionary.Count, (current, element) => (current * 397) ^ valueComparer.GetHashCode(element.Value));
+    }
+}
---
+++ Vostok.Commons.Helpers/Comparers/ListComparer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vostok.Commons.Helpers.Comparers
+{
+    internal class ListComparer<T> : IEqualityComparer<IReadOnlyList<T>>
+    {
+        public static readonly ListComparer<T> Instance = new ListComparer<T>();
+
+        private readonly IEqualityComparer<T> elementComparer;
+
+        public ListComparer(IEqualityComparer<T> elementComparer = null)
+        {
+            this.elementComparer = elementComparer ?? EqualityComparer<T>.Default;
+        }
+
+        public bool Equals(IReadOnlyList<T> x, IReadOnlyList<T> y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+
+            if (x == null || y == null)
+                return false;
+
+            if (x.Count != y.Count)
+                return false;
+
+            for (var i = 0; i < x.Count; i++)
+            {
+                if (!elementComparer.Equals(x[i], y[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+        public int GetHashCode(IReadOnlyList<T> list)
+            => list == null ? 0 : list.Aggregate(list.Count, (current, element) => (current * 397) ^ elementComparer.GetHashCode(element));
+    }
+}
