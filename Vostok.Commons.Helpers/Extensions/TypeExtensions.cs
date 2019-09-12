64cc70ae973d131820bf10496be4f965cdf2a740
macart <macart@skbkontur.ru>
2019-09-12 14:10:02 +0500

    add TypeExtensions

---
+++ Vostok.Commons.Helpers/Extensions/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Vostok.Commons.Helpers.Extensions
+{
+    internal static class TypeExtensions
+    {
+        public static bool IsNullable(this Type type) => Nullable.GetUnderlyingType(type) != null;
+
+        public static IEnumerable<PropertyInfo> GetInstanceProperties(this Type type) =>
+            type.GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(p => p.GetIndexParameters().Length == 0);
+
+        public static IEnumerable<FieldInfo> GetInstanceFields(this Type type) =>
+            type.GetFields(BindingFlags.Instance | BindingFlags.Public);
+    }
+}
