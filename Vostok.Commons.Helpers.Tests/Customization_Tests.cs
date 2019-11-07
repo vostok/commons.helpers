66c4241eafd7207e6863d615af5c2b1eae596c1b
kungurtsev <kungurtsev@skbkontur.ru>
2019-11-07 13:32:41 +0500

    Customization helper

---
+++ Vostok.Commons.Helpers.Tests/Customization_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Helpers.Tests
+{
+    [TestFixture]
+    internal class Customization_Tests
+    {
+        [Test]
+        public void Should_perform_customize_action_in_order()
+        {
+            var customization = new Customization<List<int>>();
+
+            customization.AddCustomization(
+                l => { l.Add(1); });
+
+            customization.AddCustomization(
+                l =>
+                {
+                    var ll = new List<int>(l) { 2 };
+                    return ll;
+                });
+
+            customization.AddCustomization(
+                l => { l.Add(3); });
+
+            customization.AddCustomization(
+                l =>
+                {
+                    var ll = new List<int>(l) { 4 };
+                    return ll;
+                });
+
+            var list = customization.Customize(new List<int> { 0 });
+
+            list.Should().BeEquivalentTo(new List<int> { 0, 1, 2, 3, 4 }, options => options.WithStrictOrdering());
+        }
+    }
+}
