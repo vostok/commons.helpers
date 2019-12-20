3253f44e1b1085deeaa5c08b5ff05d67d7996df8
kungurtsev <kungurtsev@skbkontur.ru>
2019-12-20 16:03:24 +0500

    CancellationTokenExtensions

---
+++ Vostok.Commons.Helpers/Extensions/CancellationTokenExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vostok.Commons.Helpers.Extensions
+{
+    internal static class CancellationTokenExtensions
+    {
+        public static async Task WaitAsync(this CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            using (cancellationToken.Register(o => ((TaskCompletionSource<bool>)o).TrySetCanceled(), tcs))
+            {
+                await tcs.Task;
+            }
+        }
+    }
+}
