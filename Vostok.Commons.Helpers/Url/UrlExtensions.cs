3ff7f05985fec26b54d93f38793d0fb611e79094
kungurtsev <kungurtsev@skbkontur.ru>
2019-12-23 14:39:44 +0500

    ToStringWithoutQuery(str)

--- Vostok.Commons.Helpers/Url/UrlExtensions.cs
+++ Vostok.Commons.Helpers/Url/UrlExtensions.cs
@@ -4,10 +4,11 @@ namespace Vostok.Commons.Helpers.Url
 {
     internal static class UrlExtensions
     {
-        public static string ToStringWithoutQuery(this Uri url)
-        {
-            var urlString = url.ToString();
+        public static string ToStringWithoutQuery(this Uri url) =>
+            ToStringWithoutQuery(url.ToString());
 
+        public static string ToStringWithoutQuery(string urlString)
+        {
             var queryBeginning = urlString.IndexOf("?", StringComparison.Ordinal);
             if (queryBeginning >= 0)
                 urlString = urlString.Substring(0, queryBeginning);
