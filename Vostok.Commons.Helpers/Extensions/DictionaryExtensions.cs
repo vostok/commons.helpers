92b3b06265927228c37e0b5b6db06335f5b3e787
kungurtsev <kungurtsev@skbkontur.ru>
2020-05-14 12:50:44 +0500

    + DictionaryExtensions

---
+++ Vostok.Commons.Helpers/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Vostok.Commons.Helpers.Extensions
+{
+    internal static class DictionaryExtensions
+    {
+        public static TValue GetValueOrNull<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : class =>
+            dictionary.TryGetValue(key, out var value) ? value : null;
+    }
+}
