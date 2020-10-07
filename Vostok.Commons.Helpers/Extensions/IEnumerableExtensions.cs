fbaccf5d9a60b82487ff0fe46ae1ca75c2fa2a9b
iloktionov <iloktionov@skbkontur.ru>
2020-10-07 12:43:03 +0500

    Added missing ConfigureAwait(false).

--- Vostok.Commons.Helpers/Extensions/IEnumerableExtensions.cs
+++ Vostok.Commons.Helpers/Extensions/IEnumerableExtensions.cs
@@ -8,6 +8,6 @@ namespace Vostok.Commons.Helpers.Extensions
     internal static class IEnumerableExtensions
     {
         public static async Task<IEnumerable<TOut>> LoopAsync<TIn, TOut>(this IEnumerable<TIn> enumerable, Func<TIn, Task<TOut>> selector) =>
-            await Task.WhenAll(enumerable.Select(selector));
+            await Task.WhenAll(enumerable.Select(selector)).ConfigureAwait(false);
     }
 }
