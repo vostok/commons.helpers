21b3367817f414b1fe3f5a0847b6297b1157c40f
deniaa <deniaa@skbkontur.ru>
2021-11-26 15:53:46 +0500

    Add ICachingObservable<T> interface and new implementation - LockFreeReadingCachingObservable.

--- Vostok.Commons.Helpers.Tests/Observable/CachingObservable_Tests.cs
+++ Vostok.Commons.Helpers.Tests/Observable/CachingObservable_Tests.cs
@@ -7,9 +7,28 @@ using Vostok.Commons.Helpers.Observable;
 namespace Vostok.Commons.Helpers.Tests.Observable
 {
     [TestFixture]
-    internal class CachingObservable_Tests
+    internal class CachingObservable_Tests : CachingObservable_Tests_Base
     {
-        private CachingObservable<string> observable;
+        [SetUp]
+        public void SetUp()
+        {
+            Observable = new CachingObservable<string>();
+        }
+    }
+    [TestFixture]
+    internal class LockFreeReadingCachingObservable_Tests : CachingObservable_Tests_Base
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Observable = new LockFreeReadingCachingObservable<string>();
+        }
+    }
+
+    internal abstract class CachingObservable_Tests_Base
+    {
+        protected ICachingObservable<string> Observable;
+
         private IObserver<string> observer1;
         private IObserver<string> observer2;
         private Exception error1;
@@ -18,8 +37,6 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [SetUp]
         public void TestSetup()
         {
-            observable = new CachingObservable<string>();
-
             observer1 = Substitute.For<IObserver<string>>();
             observer2 = Substitute.For<IObserver<string>>();
 
@@ -30,7 +47,7 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Subscribe_should_do_nothing_when_nothing_has_been_observed_yet()
         {
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
             observer1.ReceivedCalls().Should().BeEmpty();
         }
@@ -38,9 +55,9 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Subscribe_should_pass_initial_value_to_observer_immediately()
         {
-            observable = new CachingObservable<string>("initial");
+            Observable = new CachingObservable<string>("initial");
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
             observer1.ReceivedCalls().Should().HaveCount(1);
             observer1.Received().OnNext("initial");
@@ -49,10 +66,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Subscribe_should_pass_latest_value_to_observer_immediately()
         {
-            observable.Next("1");
-            observable.Next("2");
+            Observable.Next("1");
+            Observable.Next("2");
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
             observer1.ReceivedCalls().Should().HaveCount(1);
             observer1.Received().OnNext("2");
@@ -61,12 +78,12 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Subscribe_should_pass_latest_value_to_observer_immediately_even_after_completee()
         {
-            observable.Next("1");
-            observable.Next("2");
+            Observable.Next("1");
+            Observable.Next("2");
 
-            observable.Complete();
+            Observable.Complete();
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
             observer1.ReceivedCalls().Should().HaveCount(2);
             observer1.Received().OnNext("2");
@@ -76,10 +93,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Subscribe_should_cause_the_observer_to_receive_all_later_updates()
         {
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Next("1");
-            observable.Next("2");
+            Observable.Next("1");
+            Observable.Next("2");
 
             observer1.ReceivedCalls().Should().HaveCount(2);
             observer1.Received().OnNext("1");
@@ -89,10 +106,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Error_should_complete_all_current_observers()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2);
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2);
 
-            observable.Error(error1);
+            Observable.Error(error1);
 
             observer1.Received().OnError(error1);
             observer2.Received().OnError(error1);
@@ -101,11 +118,11 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Error_should_do_nothing_when_another_error_has_been_seen_earlier()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2);
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2);
 
-            observable.Error(error1);
-            observable.Error(error2);
+            Observable.Error(error1);
+            Observable.Error(error2);
 
             observer1.DidNotReceive().OnError(error2);
             observer2.DidNotReceive().OnError(error2);
@@ -114,12 +131,12 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Error_should_cause_subsequent_next_calls_to_have_no_effect()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2);
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2);
 
-            observable.Error(error1);
+            Observable.Error(error1);
 
-            observable.Next("value");
+            Observable.Next("value");
 
             observer1.DidNotReceive().OnNext("value");
             observer2.DidNotReceive().OnNext("value");
@@ -128,11 +145,11 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Error_should_prevent_any_further_subscriptions()
         {
-            observable.Error(error1);
+            Observable.Error(error1);
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Next("value");
+            Observable.Next("value");
 
             observer1.DidNotReceive().OnNext("value");
         }
@@ -140,11 +157,11 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Error_should_prevent_further_complete()
         {
-            observable.Error(error1);
+            Observable.Error(error1);
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Complete();
+            Observable.Complete();
 
             observer1.DidNotReceive().OnCompleted();
         }
@@ -152,9 +169,9 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Error_should_cause_all_new_subscribers_to_immediately_complete_with_error()
         {
-            observable.Error(error1);
+            Observable.Error(error1);
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
             observer1.Received().OnError(error1);
         }
@@ -162,10 +179,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Next_should_propagate_new_values_to_all_subscribed_observers()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2);
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2);
 
-            observable.Next("1");
+            Observable.Next("1");
 
             observer1.Received().OnNext("1");
             observer2.Received().OnNext("1");
@@ -174,10 +191,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Next_should_send_nulls()
         {
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Next(null);
-            observable.Next("1");
+            Observable.Next(null);
+            Observable.Next("1");
 
             observer1.Received().OnNext(null);
             observer1.Received().OnNext("1");
@@ -186,10 +203,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Complete_should_complete_all_current_observers()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2);
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2);
 
-            observable.Complete();
+            Observable.Complete();
 
             observer1.Received().OnCompleted();
             observer2.Received().OnCompleted();
@@ -198,11 +215,11 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Complete_should_prevent_any_further_subscriptions()
         {
-            observable.Complete();
+            Observable.Complete();
 
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Next("value");
+            Observable.Next("value");
 
             observer1.DidNotReceive().OnNext("value");
         }
@@ -210,11 +227,11 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Complete_should_prevent_any_further_events()
         {
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Complete();
+            Observable.Complete();
 
-            observable.Next("value");
+            Observable.Next("value");
 
             observer1.DidNotReceive().OnNext("value");
         }
@@ -222,11 +239,11 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Complete_should_send_value()
         {
-            observable.Subscribe(observer1);
+            Observable.Subscribe(observer1);
 
-            observable.Complete("complete");
+            Observable.Complete("complete");
 
-            observable.Next("value");
+            Observable.Next("value");
 
             observer1.Received().OnNext("complete");
             observer1.DidNotReceive().OnNext("value");
@@ -235,10 +252,10 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Disposing_a_subscription_should_cause_the_subscriber_to_stop_getting_notifications()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2).Dispose();
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2).Dispose();
 
-            observable.Next("1");
+            Observable.Next("1");
 
             observer1.Received().OnNext("1");
             observer2.DidNotReceive().OnNext("1");
@@ -247,14 +264,14 @@ namespace Vostok.Commons.Helpers.Tests.Observable
         [Test]
         public void Exception_in_one_of_the_observers_should_not_prevent_invocation_of_the_rest()
         {
-            observable.Subscribe(observer1);
-            observable.Subscribe(observer2);
+            Observable.Subscribe(observer1);
+            Observable.Subscribe(observer2);
 
             observer1
                 .WhenForAnyArgs(o => o.OnNext(default))
                 .Throw(new Exception());
 
-            observable.Next("1");
+            Observable.Next("1");
 
             observer1.Received().OnNext("1");
             observer2.Received().OnNext("1");
