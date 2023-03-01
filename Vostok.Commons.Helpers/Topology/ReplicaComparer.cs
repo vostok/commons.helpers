c90448af04db37f5a728206dc3b1e522fb4dd242
lunev.d <lunev.d@kontur.ru>
2023-03-01 19:21:00 +0500

    Move IsIpAddress method to ReplicaComparer class for backward compability

--- Vostok.Commons.Helpers/Topology/ReplicaComparer.cs
+++ Vostok.Commons.Helpers/Topology/ReplicaComparer.cs
@@ -66,10 +66,52 @@ namespace Vostok.Commons.Helpers.Topology
             if (dotIndex < 0)
                 return;
 
-            if (IpAddressDefiner.IsIpAddress(host))
+            if (IsIpAddress(host))
                 return;
 
             length = dotIndex;
+        }
+        
+        private static bool IsIpAddress(string host)
+        {
+            var numParts = 0;
+            var currentOctetValue = 0;
+            var hasAtLeastOneDigitInOctet = false;
+
+            foreach (var c in host)
+            {
+                // note (lunev.d, 01.03.2023): We don't support port specification at the end of address
+                // and so we can consider that if an address contains colon - it's an IPv6 address
+                if (c == ':')
+                    return true;
+
+                if (c == '.')
+                {
+                    if (numParts >= 3 || !hasAtLeastOneDigitInOctet)
+                        return false;
+
+                    currentOctetValue = 0;
+                    numParts++;
+                    hasAtLeastOneDigitInOctet = false;
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    var digit = c - '0';
+                    currentOctetValue = currentOctetValue * 10 + digit;
+                    
+                    if (currentOctetValue > 255)
+                        return false;
+                    
+                    hasAtLeastOneDigitInOctet = true;
+                }
+                else
+                    return false;
+            }
+
+            if (numParts > 3 || !hasAtLeastOneDigitInOctet)
+                return false;
+
+            return true;
         }
     }
 }
