d24be41a4a7cce86603248c2cee7f19eb99245dd
deniaa <deniaa@skbkontur.ru>
2023-04-10 14:13:24 +0500

    refactoring in StreamLinesReader

--- Vostok.Commons.Helpers/Spans/StreamLinesReader.cs
+++ Vostok.Commons.Helpers/Spans/StreamLinesReader.cs
@@ -1,32 +1,29 @@
-﻿
-using JetBrains.Annotations;
-#if NETCOREAPP3_1_OR_GREATER
+﻿#if NETCOREAPP3_1_OR_GREATER
 using System;
 using System.Buffers;
 using System.IO;
 using System.Text.Unicode;
-#endif
+using JetBrains.Annotations;
 
 namespace Vostok.Commons.Helpers.Spans;
 
-#if NETCOREAPP3_1_OR_GREATER
 /// <summary>
 /// string reader with zero garbage
 /// TryReadLine call invalidates content of previously returned lines (ReadOnlySpan's)!!
-/// allocates internal char[] buffer. is's size not greater than (maximum line length + 20%)
+/// allocates internal char[] buffer. it's size not greater than (maximum line length + 20%)
 /// uses UTF-8 encoding for char conversion
 /// line delimiters are only "\n" or "\r" or "\r\n" (exactly same as StreamReader)
 /// </summary>
 [PublicAPI]
 internal class StreamLinesReader : IDisposable
 {
-    private const double growFactor = 1.2;
-    private const int maxBytesForChar = 4;
+    private const double GrowFactor = 1.2;
+    private const int MaxBytesForChar = 4;
     private readonly byte[] bytesBuffer;
     private readonly bool ownsStream;
     private readonly Stream stream;
     private int bytesBufferLength;
-    private int bytesReadFromBuffer;
+    private int bytesBufferBytesRead;
 
     private char[] charBuffer; //charBuffer contains chars from 'start' of line. cant contain more than 1 line
     private int charBufferCharsRead;
@@ -42,8 +39,8 @@ internal class StreamLinesReader : IDisposable
     /// if byteBufferSize is small, stream reading is not effective, and chars converted via small chunks
     /// </summary>
     /// <param name="stream">source stream</param>
-    /// <param name="byteBufferSize">buffer size fro stream reading.</param>
-    /// <param name="charsBufferSize">initial char bufferr size for string conversion. resized up to 1.2*(max_string_length + 1)</param>
+    /// <param name="byteBufferSize">buffer size for stream reading.</param>
+    /// <param name="charsBufferSize">initial char buffer size for string conversion. resized up to 1.2*(max_string_length + 1)</param>
     /// <param name="ownsStream">if true, Dispose will call stream.Dispose</param>
     public StreamLinesReader(
         Stream stream,
@@ -51,8 +48,8 @@ internal class StreamLinesReader : IDisposable
         int charsBufferSize = 100,
         bool ownsStream = true)
     {
-        if (byteBufferSize < maxBytesForChar)
-            byteBufferSize = maxBytesForChar; //must fit 1 char UTF8
+        if (byteBufferSize < MaxBytesForChar)
+            byteBufferSize = MaxBytesForChar; //must fit 1 char UTF8
 
         if (charsBufferSize <= 2)
             charsBufferSize = 2; //safe: 2-char code point should fit
@@ -65,10 +62,10 @@ internal class StreamLinesReader : IDisposable
     }
 
     /// <summary>
-    /// true if end of stream is reached.
-    /// becames true after TryReadLine firstly returns true
+    /// True if end of stream is reached.
+    /// Becomes true after TryReadLine firstly returns true.
     /// </summary>
-    public bool IsEOF { get; private set; }
+    public bool IsEndOfStream { get; private set; }
 
     public void Dispose()
     {
@@ -93,16 +90,17 @@ internal class StreamLinesReader : IDisposable
     public void ResetBuffers()
     {
         bytesBufferLength = 0;
-        bytesReadFromBuffer = 0;
+        bytesBufferBytesRead = 0;
 
-        charBufferCharsRead = 0;
         charBufferLength = 0;
+        charBufferCharsRead = 0;
+
         charBufferStringStart = 0;
         consumeN = false;
 
         resizeAllowed = true;
         lastByteRead = false;
-        IsEOF = false;
+        IsEndOfStream = false;
     }
 
     /// <summary>
@@ -112,12 +110,12 @@ internal class StreamLinesReader : IDisposable
     /// <returns></returns>
     public bool TryReadLine(out ReadOnlySpan<char> result)
     {
-        while (!IsEOF)
+        while (!IsEndOfStream)
         {
             ReadNextData();
             var nextStringStart = -1;
 
-            if (IsEOF)
+            if (IsEndOfStream)
                 if (charBufferStringStart < charBufferLength)
                     nextStringStart = charBufferLength + 1;
 
@@ -192,19 +190,16 @@ internal class StreamLinesReader : IDisposable
                 //do resize
 
                 var old = charBuffer;
-                var newLength =
-                    Math.Max((int)(old.Length * growFactor), old.Length + 2); //new buffer should be larger.
+                var newLength = Math.Max((int)(old.Length * GrowFactor), old.Length + 2); //new buffer should be larger.
                 charBuffer = new char[newLength];
-                srcToCopy = new ReadOnlySpan<char>(old, 0, charBufferLength);
+                srcToCopy = new ReadOnlySpan<char>(old);
             } //else - no need to resize. maybe bytesBuffer is small, convert bytes to [charBufferLength, charBuffer.Length)
         }
         else
         {
             //dont resize, shift buffer.
             //copy from [charBufferStringStart to charBufferLength)
-            srcToCopy = new ReadOnlySpan<char>(charBuffer,
-                charBufferStringStart,
-                charBufferLength - charBufferStringStart);
+            srcToCopy = new ReadOnlySpan<char>(charBuffer, charBufferStringStart, charBufferLength - charBufferStringStart);
             resizeAllowed = true;
             charBufferCharsRead -= charBufferStringStart;
             charBufferLength -= charBufferStringStart;
@@ -215,37 +210,37 @@ internal class StreamLinesReader : IDisposable
             srcToCopy.CopyTo(new Span<char>(charBuffer));
 
         var status = Utf8.ToUtf16(
-            new ReadOnlySpan<byte>(bytesBuffer, bytesReadFromBuffer, bytesBufferLength - bytesReadFromBuffer),
+            new ReadOnlySpan<byte>(bytesBuffer, bytesBufferBytesRead, bytesBufferLength - bytesBufferBytesRead),
             new Span<char>(charBuffer, charBufferLength, charBuffer.Length - charBufferLength),
             out var bytesConverted,
             out var charsConverted,
             false,
             false);
 
-        bytesReadFromBuffer += bytesConverted;
+        bytesBufferBytesRead += bytesConverted;
         charBufferLength += charsConverted;
 
         if (status == OperationStatus.InvalidData)
             FailBadBytes();
 
         if (lastByteRead)
-            IsEOF = status == OperationStatus.Done && charsConverted == 0; //also should be bytesConverted==0
+            IsEndOfStream = status == OperationStatus.Done && charsConverted == 0; //also should be bytesConverted==0
     }
 
     private void ReadNextBytes()
     {
-        if (bytesReadFromBuffer + maxBytesForChar <
-            bytesBufferLength) //not all buffer drained, do nothing. can convert >=1 valid char
+        if (bytesBufferBytesRead + MaxBytesForChar < bytesBufferLength) //not all buffer drained, do nothing. can convert >=1 valid char
             return;
-        if (bytesReadFromBuffer < bytesBufferLength) //something remains. max 2 bytes for UTF8
+
+        if (bytesBufferBytesRead < bytesBufferLength) //something remains. max 2 bytes for UTF8
         {
             //copy to beginning
-            new Span<byte>(bytesBuffer, bytesReadFromBuffer, bytesBufferLength - bytesReadFromBuffer).CopyTo(
-                new Span<byte>(bytesBuffer));
+            new Span<byte>(bytesBuffer, bytesBufferBytesRead, bytesBufferLength - bytesBufferBytesRead)
+                .CopyTo(new Span<byte>(bytesBuffer));
         }
 
-        bytesBufferLength -= bytesReadFromBuffer;
-        bytesReadFromBuffer = 0; //copied bytes will be re-read while char conversion
+        bytesBufferLength -= bytesBufferBytesRead;
+        bytesBufferBytesRead = 0; //copied bytes will be re-read while char conversion
 
         var bytesRead = stream.Read(bytesBuffer, bytesBufferLength, bytesBuffer.Length - bytesBufferLength);
         if (bytesRead == 0)
@@ -254,7 +249,7 @@ internal class StreamLinesReader : IDisposable
         bytesBufferLength += bytesRead;
     }
 
-    private void FailBadBytes()
+    private static void FailBadBytes()
     {
         throw new InvalidOperationException("Cant convert bytes to chars via UTF8");
     }
